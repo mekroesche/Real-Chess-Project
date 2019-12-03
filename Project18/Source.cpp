3c19704270beb613b0be5c55cd0c692ffae5e53f
mekroesche <mekroesche@users.noreply.github.com>
2019-12-02 21:58:53 -0600

    Add project files.

---
+++ Project18/Source.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+//using namespace std;
+//this is a comment yee yeet
+
+void main(){
+	int value;
+	std::cout << "hello world. how much do you hate yourself?\n";
+	std::cin >> value;
+	for (int i = 0; i < value; i++) {
+		std::cout << "###   ###   ###   ###   \n";
+		std::cout << "###   ###   ###   ###   \n";
+		std::cout << "   ###   ###   ###   ###\n";
+		std::cout << "   ###   ###   ###   ###\n";
+	}
+	int test;
+	std::cin >> test;
+}
